d68f4f265d73d9d94f9458e1aa7bffe81675abf7
furest <yannick.diepart@hotmail.com>
2019-04-10 23:24:42 +0200

    Refactor daemon

--- labDaemon/labDaemon.py
+++ labDaemon/labDaemon.py
@@ -19,13 +19,30 @@ def listTwinings(client_id, dbParams):
     db = mysql.connector.connect(**dbParams)
     c = db.cursor(dictionary=True)
     client = (client_id, client_id)
-    c.execute("SELECT a1.user_login AS login1, a1.user_email AS email1, a1.id AS id1, a2.user_login AS login2, a2.user_email AS email2, a2.id AS id2 FROM wp_twinings INNER JOIN wp_users a1 ON a1.id = wp_twinings.academy_1 INNER JOIN wp_users a2 ON a2.id = wp_twinings.academy_2 WHERE a1.user_email = %s OR a2.user_email = %s ", client)
+#    c.execute("SELECT a1.user_login AS a1_login, a1.user_email AS a1_email, a1.id AS a1_id, a2.user_login AS a2_login, a2.user_email AS a2_email, a2.id AS a2_id, wp_twinings.id AS twining_id FROM wp_twinings INNER JOIN wp_users a1 ON a1.id = wp_twinings.academy_1 INNER JOIN wp_users a2 ON a2.id = wp_twinings.academy_2 WHERE a1.user_email = %s OR a2.user_email = %s ", client)
+    c.execute("SELECT a1.user_login AS login,\
+                    a1.user_email AS email,\
+                    a1.id AS academy_id,\
+                    wp_twinings.id AS twining_id\
+            FROM wp_twinings\
+            INNER JOIN wp_users a1 ON a1.id = wp_twinings.academy_1\
+            INNER JOIN wp_users a2 ON a2.id = wp_twinings.academy_2\
+            WHERE a2.user_email =  %s\
+            UNION ALL\
+            SELECT a2.user_login AS login,\
+                    a2.user_email AS email,\
+                    a2.id AS academy_id,\
+                    wp_twinings.id AS twining_id\
+            FROM wp_twinings\
+            INNER JOIN wp_users a1 ON a1.id = wp_twinings.academy_1\
+            INNER JOIN wp_users a2 ON a2.id = wp_twinings.academy_2\
+            WHERE a1.user_email =  %s", client)
     twlist = c.fetchall()
     return twlist 
 
 def errorRoutine(writable, message):
     jsonError = { "error":True, "message":message}
-    writable.write(json.dumps(jsonError))
+    writable.write(json.dumps(jsonError).encode())
 
 def retrieveUser(dbParams, address):
     db = mysql.connector.connect(**dbParams)
@@ -54,9 +71,12 @@ class labRequestHandler(socketserver.StreamRequestHandler):
         #Converting the json string to python dict
         try:
             dictData = json.loads(stringData)
-        except JSONDecodeError:
+        except json.decoder.JSONDecodeError:
             errorRoutine(self.wfile,"Request is not a JSON object")
             return
+        except:
+            errorRoutine(self.wfile, "An unknown error occured")
+            return 
         if not "type" in dictData:
             errorRoutine(self.wfile, "Request does not contain a type")
             return
