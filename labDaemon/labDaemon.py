3333b536ad39c58c9cba48ec305d8c52b260aeff
furest <yannick.diepart@hotmail.com>
2019-04-10 02:00:07 +0200

    Server listing script

---
+++ labDaemon/labDaemon.py
@@ -0,0 +1,83 @@
+import json
+import socketserver
+from threading import *
+import mysql.connector
+
+tbParams = {
+        "host": 'localhost',
+        "user":'vpn',
+        "passwd":'vpnpassword',
+        "database":'twinbridge'
+    }
+wpParams = {
+        "host": 'localhost',
+        "user":'vpn',
+        "passwd":'vpnpassword',
+        "database":'wordpress'
+    }
+def listTwinings(client_id, dbParams):
+    db = mysql.connector.connect(**dbParams)
+    c = db.cursor(dictionary=True)
+    client = (client_id, client_id)
+    c.execute("SELECT a1.user_login AS login1, a1.user_email AS email1, a1.id AS id1, a2.user_login AS login2, a2.user_email AS email2, a2.id AS id2 FROM wp_twinings INNER JOIN wp_users a1 ON a1.id = wp_twinings.academy_1 INNER JOIN wp_users a2 ON a2.id = wp_twinings.academy_2 WHERE a1.user_email = %s OR a2.user_email = %s ", client)
+    twlist = c.fetchall()
+    return twlist 
+
+def errorRoutine(writable, message):
+    jsonError = { "error":True, "message":message}
+    writable.write(json.dumps(jsonError))
+
+def retrieveUser(dbParams, address):
+    db = mysql.connector.connect(**dbParams)
+    c = db.cursor(dictionary=True)
+    ipClient = (address,)
+    c.execute("SELECT * FROM CONNECTED_CLIENTS WHERE VIRT_IP = %s", ipClient)
+    client = c.fetchone()
+    return client
+
+
+
+class labRequestHandler(socketserver.StreamRequestHandler):
+    """
+    Handles the requests.
+    self.request = socket to the client
+    self.server = the server
+    self.client_address = client's address
+    self.rfile/self.wfile = file like 
+    """
+
+    def handle(self):
+        print("handling client",self.client_address)
+        #Reads the data sent by the client until the first \n
+        stringData = self.rfile.readline().strip()
+        print("received ", stringData)
+        #Converting the json string to python dict
+        try:
+            dictData = json.loads(stringData)
+        except JSONDecodeError:
+            errorRoutine(self.wfile,"Request is not a JSON object")
+            return
+        if not "type" in dictData:
+            errorRoutine(self.wfile, "Request does not contain a type")
+            return
+
+        user = retrieveUser(tbParams, self.client_address[0])
+        if(user == None):
+            errorRoutine("IP is not bound to a user")
+            return
+        if dictData["type"] == "list":
+            twiningsList = listTwinings(user["username"], wpParams)
+            print("list twinings:", twiningsList)
+            twiningsListStr = json.dumps(twiningsList) + "\n"
+            self.wfile.write(twiningsListStr.encode())
+            return
+        else:
+            errorRoutine("unknown request type")
+
+if __name__ == "__main__":
+    
+    HOST, PORT = "172.16.100.1", 1500
+    
+    server = socketserver.ThreadingTCPServer((HOST, PORT), labRequestHandler)
+    print("Server created. Now serving")
+    server.serve_forever()
